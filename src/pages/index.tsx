33f9fdda17038c0f294f52a9096551181ad35ff8
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-08-12 10:01:46 -0300

    .
    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import styles from '../styles/home.module.scss'
+import styles from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
   return (
-   
-    <h1 className={styles.title} > Testando</h1>
- 
-  )
+    <>
+      <Head>
+        <title>Home | ignews</title>
+      </Head>
+      <h1 className={styles.title}> Testando</h1>
+    </>
+  );
 }
