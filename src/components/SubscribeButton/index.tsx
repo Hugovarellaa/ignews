4ff5aed9afe9ac38c0ace49a62f87711e3cb538a
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-08-20 20:32:34 -0300

    .
    .

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from "next-auth/client";
+import router, { useRouter } from "next/router";
 import { api } from "../../service/api";
 import { getStripeJs } from "../../service/stripe-js";
 import styles from "./styles.module.scss";
@@ -9,23 +10,26 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
   
   async function handleSubscribe() {
     if(!session) {
       signIn('github')
       return;
-    }else{
-      
+    }
+      if(session.activeSubscription){
+        router.push('/posts')
+        return;
+      }
       try{
         const response = await api.post('/subscribe')
         const { sessionId } = response.data
-
         const stripe = await getStripeJs()
         await stripe.redirectToCheckout({sessionId})
       }catch(err){
         alert(err.message)
       }
-    }
+    
   }
   return (
     <button
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { fauna } from "../../../service/fauna";
 import { query as q } from "faunadb";
+import { session } from "next-auth/client";
 
 export default NextAuth({
   providers: [
@@ -11,6 +12,41 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try{
+        session.user.email;
+
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
       try {
