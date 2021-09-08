21a0dc360f37f1da071434f71874bfa63777740f
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-08 10:06:52 -0300

    .
    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-import styles from "../styles/home.module.scss";
+import Head  from "next/head";
 
 export default function Home() {
-  return <h1 className={styles.title}>Ola</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | Ig.news</title>
+      </Head>
+      <h1 >Ola</h1>
+    </>
+  );
 }
