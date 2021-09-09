f021f142e27e90ce2ec4afbbc5841f35ef6ea3e1
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-09 12:50:41 -0300

    Update [...nextauth].tsx
    .

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import { session } from "next-auth/client";
 import Providers from "next-auth/providers";
 
 export default NextAuth({
@@ -7,7 +8,13 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
   ],
-})
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log(user)
+      return true
+    },
+  }
+});
