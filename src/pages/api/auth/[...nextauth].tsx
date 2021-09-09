54bbcc6fe292c68a3d71274cbd9bb0f09d6d6325
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-09 17:30:40 -0300

    Update [...nextauth].tsx

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { fauna } from "../../../services/fauna";
-import { query as q } from "faunadb";
+import { Casefold, query as q } from "faunadb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -17,7 +17,28 @@ export default NextAuth({
       const { email } = user;
 
       try {
-        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
         return true;
       } catch {
         return false;
