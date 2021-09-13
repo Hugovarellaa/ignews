2e720d976d74fd2624eb7d64a98c876424db8f8c
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-13 11:41:29 -0300

    .
    .

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,30 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from "next-auth/client";
+import styles from "./styles.module.scss";
+
 export function SignInButton() {
-  const isLog = true;
-  return isLog ? (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="var(--green-500)"/>
-      Hugo Varella
-      <FiX  color="var(--gray-200)" className={styles.closeIcon}/>
+  const [session] = useSession();
+  console.log(session);
+  
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="var(--green-500)" />
+      {session.user.name}
+      <FiX color="var(--gray-200)" className={styles.closeIcon} />
     </button>
-  ) :(
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="var(--yellow-500)"/>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="var(--yellow-500)" />
       Sign in with Github
     </button>
-  )
+  );
 }
