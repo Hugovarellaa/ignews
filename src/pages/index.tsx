fcd1d742a37c66441f1c44b94af177ead525d60c
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-13 09:00:45 -0300

    .
    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
-
+import styles from "./home.module.scss";
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | Ig.news</title>
       </Head>
-      <h1>Testando</h1>
+
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <div>
+            <span>👏 Hey, welcome</span>
+            <h1>
+              News about <br /> the <span>React</span> world.
+            </h1>
+            <p>
+              Get acess to all the publications <br />
+              <span>for $9.90 month</span>
+            </p>
+          </div>
+        </section>
+          <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
