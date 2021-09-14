ebfb7cffce2ec81c125ab0b7b573708fff57bdcc
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-14 16:29:59 -0300

    Login Github
    Login Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,21 @@
 import { FaGithub } from "react-icons/fa";
-import styles from './styles.module.scss' ;
+import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/client";
 import { FiX } from "react-icons/fi";
 
-
 export function SignInButton() {
-  const isLog = true
-  
-  return isLog ? (
-    <button type="button" className={styles.signInButton}>
+  const [session] = useSession()
+
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="var(--green-500)" />
-      Hugo Alves Varella
-      <FiX color="var(--gray-200)" className={styles.closeIcon}/>
+      {session.user.name}
+      <FiX color="var(--gray-200)" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="var(--yellow-500)"/>
+    <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
+      <FaGithub color="var(--yellow-500)" />
       Sign in with Github
     </button>
-  )
+  );
 }
