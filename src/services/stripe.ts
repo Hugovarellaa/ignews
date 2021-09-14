7bf4d7529352794877bf78423b38ab91f3069822
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-14 14:48:30 -0300

    .
    .

---
+++ src/services/stripe.ts
@@ -0,0 +1,12 @@
+import Stripe from 'stripe';
+
+export const stripe = new Stripe(
+  process.env.STRIPE_KEY_API,
+  {
+    apiVersion: '2020-08-27',
+    appInfo: {
+      name: 'Ignews2',
+      version: "^8.174.0"
+    } 
+  }
+);
