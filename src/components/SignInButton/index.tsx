f1cb78e0aaa738daa25f61c7a5e140fba7c628ac
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-14 10:53:31 -0300

    .
    .

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,21 @@
+import { FaGithub } from "react-icons/fa";
+import styles from './styles.module.scss' ;
+import { FiX } from "react-icons/fi";
+
+
+export function SignInButton() {
+  const isLog = true
+  
+  return isLog ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="var(--green-500)" />
+      Hugo Alves Varella
+      <FiX color="var(--gray-200)" className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="var(--yellow-500)"/>
+      Sign in with Github
+    </button>
+  )
+}
