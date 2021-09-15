b9af2c1e56014d35f1d13b18ac56bc6baa0d5ae3
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-14 22:17:25 -0300

    Pagamento no Stripe
    Pagamento no Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripejs";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -8,12 +10,21 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
-    if(!session){
-      signIn('github')
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
       return;
     }
     // criação da checkout session
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout( {sessionId} );
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   return (
