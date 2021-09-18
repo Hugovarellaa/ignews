4b692ae19437b95bee63ef7e119515c48fa03763
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-18 12:00:40 -0300

    Component SubscribeButton
    Botão de inscrição e estilo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss";
 
 export default function Home() {
@@ -18,7 +19,7 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 month</span>
           </p>
-          {/* botão de inscrição */}
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
