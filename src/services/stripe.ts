cd3bd0851f37c512ef7c999edc7f0735d62ba242
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-17 21:02:43 -0300

    Delete src directory

--- src/services/stripe.ts
+++
@@ -1,9 +0,0 @@
-import Stripe from "stripe";
-
-export const stripe = new Stripe(process.env.STRIPE_KEY_API, {
-  apiVersion: "2020-08-27",
-  appInfo: {
-    name: "Ignews2",
-    version: "^8.174.0",
-  },
-});
