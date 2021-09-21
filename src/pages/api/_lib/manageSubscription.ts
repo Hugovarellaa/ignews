f7c7970e9d42a201066282e500d64c3b391a6f66
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-09-21 10:45:52 -0300

    Configurando WebHooks
    Configurando as chamadas e ouvindo pelo webhooks

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,8 @@
+export async function saveSubscription(
+  subscription: string,
+  customerId: string
+) {
+  //buscar o usuario no banco do faunadb com o ID customerId
+  //salva os dados da subscription no faunadb
+
+}
