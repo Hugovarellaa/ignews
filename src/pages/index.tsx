7d263b51eee91ea3bc2cf0ce6abdd590cf7b80cd
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-28 13:34:06 -0300

    Chapter III

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { GetServerSideProps } from "next";
 import styles from "./home.module.scss";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,12 +27,30 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>{product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JpZkyH6aihmDxYbPDNHHDEh");
+
+  const product = {
+    priceId: price.id,
+
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount /100),
+  };
+  
+  return {
+    props: {
+      product,
+    },
+  };
+};
