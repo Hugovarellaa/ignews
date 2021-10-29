82778abaee7cf6f622cf745ed1e1b1763795fb0b
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-28 23:08:51 -0300

    Chapter III
    Back-end no front-end
    
     - Usando FaunaDB

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +11,28 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET_ID,
       scope: "read:user",
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
