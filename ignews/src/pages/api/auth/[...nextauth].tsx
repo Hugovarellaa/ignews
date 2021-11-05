592355d372a04cbeb2ec9006187e0e50e6528670
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-05 16:51:52 -0300

    Usando o faunadb

--- ignews/src/pages/api/auth/[...nextauth].tsx
+++ ignews/src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth"
-import Provider from "next-auth/providers"
+import NextAuth from "next-auth";
+import Provider from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,8 +9,46 @@ export default NextAuth({
     Provider.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: "read:user"
+      scope: "read:user",
     }),
     // ...add more providers here
   ],
-})
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              {data : {email}}
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+         
+
+
+
+        return true;
+      } catch (error) {
+        console.log(`Erro na criação do usuario ${error}`);
+        return false;
+      }
+    },
+  },
+});
