5902fc8717639a796cef73384ac20cb8b9be9555
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-04 22:13:49 -0300

    .
    .

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+  <>
+    <Head>
+    <title>Home | Ignews</title>
+    </Head>
+    <main>
+      <h1>testando</h1>
+    </main>
+  </>
+  )
+}
