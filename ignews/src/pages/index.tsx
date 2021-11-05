6aeaba20eb47738ae959dc81572ea48762c6815b
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-05 10:53:21 -0300

    Home

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,14 +1,26 @@
-import Head from "next/head"
+import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
-  <>
-    <Head>
-    <title>Home | Ignews</title>
-    </Head>
-    <main>
-      <h1>testando</h1>
-    </main>
-  </>
-  )
+    <>
+      <Head>
+        <title>Home | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about <br /> the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.9 month</span>
+          </p>
+          {/* botao */}
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 }
