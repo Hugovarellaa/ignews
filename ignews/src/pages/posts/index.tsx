fb47bf4637a45c5c3c9de4f07232a94155c53d26
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-11-08 23:28:51 -0300

    Chapter III
    Front-end JAMStack - Trabalhando com CMS
    
    - Consumindo API do Prismic

---
+++ ignews/src/pages/posts/index.tsx
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import { GetStaticProps } from "next";
+import styles from "./styles.module.scss";
+import { getPrismic } from "../../services/prismic";
+import Prismic from "@prismicio/client";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.post}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismic();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "teste")],
+    {
+      fetch: ["teste.title", "teste.content"],
+      pageSize: 100,
+    }
+  );
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {},
+  };
+};
